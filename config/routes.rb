4abdeebf0bfa6034d3dfcb543631fa5f3bcd13d5
Michael <medaphyshealth@gmail.com>
2013-08-12 12:29:38 -0400

    change home page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 SampleApp::Application.routes.draw do
-  root "static_pages/home"
+  root 'static_pages#home'
   get "static_pages/home"
   get "static_pages/help"
   # The priority is based upon order of creation: first created -> highest priority.

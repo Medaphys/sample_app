f1e28fa341f302c055b192952f781ac2d7096294
Michael <medaphyshealth@gmail.com>
2013-08-13 12:33:23 -0400

    Added SSL

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,14 +5,16 @@ class UsersController < ApplicationController
   def show
   	@user = User.find(params[:id])
   end
-  def create
+ def create
     @user = User.new(user_params)
     if @user.save
+      flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
     else
       render 'new'
     end
   end
+
 
   private
 
